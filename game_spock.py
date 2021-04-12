359067f045b9181b5f216b170c8fd5735985069a
Kalelpl <36899631+Kalelpl@users.noreply.github.com>
2021-04-12 07:56:25 +0200

    Update game_spock.py
    Final Solution to the Game question

--- game_spock.py
+++ game_spock.py
@@ -3,40 +3,43 @@ import random
 points = 0
 
 def gierka():
-
-
+    global points
+    
     computer_choice = ["Rock", "Paper", "Scissor", "Lizard", "Spock"]
     x = input('Type: Rock, Paper, Scissor, Lizard, Spock\n')
 
     if x == random.choice(computer_choice):
-        print('Ok. The number of points equals ' + str( points + 1 ))
+        points = points + 1
+        print('Ok. The number of points equals ' + str( points ))
     else:
-        print('Wrong. The number of points equals ' + str(points))
+        print('Wrong. The number of points equals ' + str( points))
 
     x = input('Type: Rock, Paper, Scissor, Lizard, Spock\n')
     if x == random.choice(computer_choice):
-        print('Ok. The number of points equals ' + str(points + 1))
+        points = points + 1 
+        print('Ok. The number of points equals ' + str( points ))
     else:
-        print('Wrong. The number of points equals ' + str(points))
+        print('Wrong. The number of points equals ' + str( points ))
 
     x = input('Type: Rock, Paper, Scissor, Lizard, Spock\n')
     if x == random.choice(computer_choice):
-        print('Ok. The number of points equals ' + str(points +1))
+        points = points + 1 
+        print('Ok. The number of points equals ' + str( points ))
     else:
-        print('Wrong. The number of points equals ' + str(points))
+        print('Wrong. The number of points equals ' + str( points ))
 
+        
     if points == 3:
         print("You won, Congratulations!")
     elif points > 0:
-        print("You lost by " + str(3 - points) + ' points')
+        print("You lost by " + str(3-points) + ' points')
     elif points == 0:
         print( "You lost by 3 points")
 
     finish = input("Try again? yes or no\n")
-    while finish == "yes":
+    if finish == "yes":
         gierka()
     else:
         print("Thanks for playing")
 
 gierka()
-
